1e8360a12805967592c1ce1d89f8c83fd096d6b8
Alicea Halsted <alicea.halsted@tnc.org>
2024-03-05 10:44:50 -0500

    Load local geojson

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -3,18 +3,40 @@ import { onMounted } from "vue"
 import Map from '@arcgis/core/Map'
 import MapView from '@arcgis/core/views/MapView'
 import esriConfig from "@arcgis/core/config.js";
-// import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer"
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer"
+
 esriConfig.apiKey = 'AAPK1d9ff5650e164ac09da678fe794990ae-BAm-ie8CRQuR0X-nJcYsklC7ry026EBzGjJYLwbr3NNw7fcCpE66BS8w9uC5_pu'
     
 
   onMounted(() => {
     //set up map & associated items in composables so it can be accessed from other components
 
+    const pointsRenderer = {
+      type: "simple", // autocasts as new SimpleRenderer()
+      symbol: {
+        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+        size: 9,
+        color: '#8dc558',
+        outline: {  // autocasts as new SimpleLineSymbol()
+          color: '#ffffff',
+          width: 1  // points
+        }
+      }
+    };
     
-    // const geojsonLayer = new GeoJSONLayer({
-    //       url: "../assets/points.geojson",
-    //       copyright: "TNC"
-    //     });
+    const pointPopup = {
+        "title": "{title}",
+        "content": "{description}"
+      }
+
+
+    const geojsonLayer = new GeoJSONLayer({
+      url: "src/assets/points.geojson",
+      copyright: "TNC",
+      renderer: pointsRenderer,
+      outFields: ["title", "description"],
+      popupTemplate: pointPopup
+    });
 
     const map = new Map({
       basemap: "arcgis/topographic", // basemap styles service
@@ -27,6 +49,8 @@ esriConfig.apiKey = 'AAPK1d9ff5650e164ac09da678fe794990ae-BAm-ie8CRQuR0X-nJcYskl
       zoom: 4, // Zoom level
       container: 'map'
     });
+
+    map.add(geojsonLayer)
   })
 </script>
 
