ffccc7d1bec8cf44aae77adb14cf4097124374fa
Alicea Halsted <alicea.halsted@tnc.org>
2024-03-06 11:27:09 -0500

    Adding dialog box

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,8 @@
     }
   }
 
+  const alert = ref(false)
+
 </script>
 
 <template>
@@ -41,7 +43,7 @@
             <q-img src="./assets/wave-pattern.svg" style="height: 200px" fit="cover">
               <q-item-section>
                 <q-item-label class='header-label' color="white">COASTAL<br>Resilience<br>Mapping Portal</q-item-label>
-                <q-btn color="secondary"  text-color="white" label="Learn More" /> 
+                <q-btn color="secondary"  text-color="white" label="Learn More" @click="alert = true"/> 
               </q-item-section>
             </q-img>
           </q-item>
@@ -103,6 +105,36 @@
       </q-drawer>
       <q-btn id="drawer-toggle-close" v-show="closeDrawer" color="primary" round icon="chevron_left" @click="toggleLeftDrawer" />
       <q-btn id="drawer-toggle-open" v-show="openDrawer"  color="primary" round icon="chevron_right" @click="toggleLeftDrawer" />
+
+      <q-dialog v-model="alert">
+        <q-card>
+          <q-card-section class="row items-center q-pb-none">
+            <div class="text-h6">About the Coastal Resilience Mapping Portal</div>
+            <q-space />
+            <q-btn icon="close" flat round dense v-close-popup />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Coastal Resilience supports a community of practitioners around the world who are applying spatial planning innovations to coastal hazard risk, resilience and adaptation issues. This is a global network providing access to peer practitioners, tools, information and training focused on nature-based solutions. For more information see our <a href="https://coastalresilience.org/" target="_blank">Coastal Resilience website.</a>
+            <q-space />
+            <div class="logos">
+              <q-img class='partner-logos' src="./assets/partners/ASFPM-Logo-full-color.jpg" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/azavea-logo.png" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/cisr_logo.png" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/esri-10GlobeLogo_sRGBflat300.jpg" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/GDPC Logo Final CMYK.jpg" style="width: 200px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/IFRC.jpeg" style="width: 200px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/msft.jpg" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/NatCapLogo.png" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/NOAA_logo_color_broadcast2.jpg" style="width: 80px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/SpatialDev.jpeg" style="width: 150px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/TNCLogoPrimary_RGB2.jpg" style="width: 150px" ></q-img>
+              <q-img class='partner-logos' src="./assets/partners/UNU-EHS_3c.jpg" style="width: 150px" ></q-img>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
+
       <q-page-container>
         <MapContainer />
       </q-page-container>
@@ -113,6 +145,12 @@
 /* 1005 */
   .q-img__content > div {
     background: rgba(0, 0, 0, 0);
+  }
+  .logos {
+    text-align: center;
+  }
+  .partner-logos {
+    margin: 10px;
   }
   #drawer-toggle-close {
     z-index: 2;
